9b588cc0ac78871c91285e97ec2674c3e69c2ad4
jerry <jerrylai19990120@gmail.com>
2021-01-27 13:45:18 -0500

    slide view added

---
+++ Controllers/ChannelVC.swift
@@ -0,0 +1,30 @@
+//
+//  ChannelVC.swift
+//  Chattie
+//
+//  Created by Jerry Lai on 2021-01-27.
+//  Copyright © 2021 Jerry Lai. All rights reserved.
+//
+
+import UIKit
+
+class ChannelVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.revealViewController()?.rearViewRevealWidth = self.view.frame.size.width - 60
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Controllers/ChatVC.swift
@@ -0,0 +1,30 @@
+//
+//  ChatVC.swift
+//  Chattie
+//
+//  Created by Jerry Lai on 2021-01-27.
+//  Copyright © 2021 Jerry Lai. All rights reserved.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+    
+    
+    @IBOutlet weak var menuBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+        
+        self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
+        self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+
+}

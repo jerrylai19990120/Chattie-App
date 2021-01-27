ce59863da2b374bfcc7bd507dee8d93bae6f65a5
jerry <jerrylai19990120@gmail.com>
2021-01-27 14:51:35 -0500

    Login UI added

---
+++ Controllers/LoginVC.swift
@@ -0,0 +1,25 @@
+//
+//  LoginVC.swift
+//  Chattie
+//
+//  Created by Jerry Lai on 2021-01-27.
+//  Copyright © 2021 Jerry Lai. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+    @IBAction func closeBtnPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
---
+++ Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Chattie
+//
+//  Created by Jerry Lai on 2021-01-27.
+//  Copyright © 2021 Jerry Lai. All rights reserved.
+//
+
+import Foundation
+
+//Segues
+let TO_LOGIN = "toLogin"

d96332fb9f6502b67ce3a81165835f9d9da5a68e
jerry <jerrylai19990120@gmail.com>
2021-01-28 15:13:58 -0500

    UI update

--- Utilities/Constants.swift
+++ Utilities/Constants.swift
@@ -8,7 +8,19 @@
 
 import Foundation
 
+typealias CompletionHandler = (_ Success: Bool) -> () 
+
 //Segues
 let TO_LOGIN = "toLogin"
 let TO_SIGNUP = "toSignup"
 let UNWIND = "unwindToChannel"
+
+
+//User Defaults
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedIn"
+let USER_EMAIL = "userEmail"
+
+// URL Constants
+let BASE_URL = "https://chattie-server.herokuapp.com/"
+let URL_REGISTER = "\(BASE_URL)account/register"
